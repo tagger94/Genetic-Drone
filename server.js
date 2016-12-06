7c2e28525271109b8723e881e47f210a477c8dd0
kkohl <kohlken22@gmail.com>
2016-12-06 04:45:39 +0000

    adding server code

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ var path = require('path');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket) {
+    console.log('client connected');
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function() {
+        console.log(socket.name + ' disconnected');
+    });
+
+});
